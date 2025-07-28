5edba7a926da5826cf1dfe76de2e4c9bbe8a9c9e
AIKA <1343707921@qq.com>
2025-07-28 15:47:04 +0800

    使用Dispatcher.UIThread.Post等待数据更新完成，而不是Task.Delay

--- Antique_Tycoon/ViewModels/PageViewModels/MapEditPageViewModel.cs
+++ Antique_Tycoon/ViewModels/PageViewModels/MapEditPageViewModel.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Antique_Tycoon.Models.Cell;
 using Avalonia;
 using Avalonia.Collections;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -18,20 +19,22 @@ public partial class MapEditPageViewModel:DragAndZoomViewModel
   public Point PointerPosition { get; set; }
 
   [RelayCommand]
-  private async Task CreateEntity(string type)
+  private void CreateEntity(string type)
   {
-    await Task.Delay(10);//命令触发比menuitem的tap时间早，导致PointerPosition没更新，这样等有点💩，但目前没问题
-    switch (type)
+    Dispatcher.UIThread.Post(() =>
     {
-      case "玩家出生点":
-        MapEntities.Add(new SpawnPoint{Left = PointerPosition.X,Top = PointerPosition.Y,Title = "玩家出生点"});
-        break;
-      case "地产":
-        MapEntities.Add(new Estate{Left = PointerPosition.X,Top = PointerPosition.Y,Title = "某生态群系"});
-        break;
-      case "自定义事件":
-        break;
-    }
+      switch (type)
+          {
+            case "玩家出生点":
+              MapEntities.Add(new SpawnPoint{Left = PointerPosition.X,Top = PointerPosition.Y,Title = "玩家出生点"});
+              break;
+            case "地产":
+              MapEntities.Add(new Estate{Left = PointerPosition.X,Top = PointerPosition.Y,Title = "某生态群系"});
+              break;
+            case "自定义事件":
+              break;
+          }
+    }, DispatcherPriority.Render);
   }
 
   [RelayCommand]
