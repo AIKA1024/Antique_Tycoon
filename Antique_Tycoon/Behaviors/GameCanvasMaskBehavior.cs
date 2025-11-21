2f608d85e0c0e02146306745a194cc6051f9f1fc
AIKA1024 <1343707921@qq.com>
2025-11-21 11:39:55 +0800

    添加遮罩行为

---
+++ Antique_Tycoon/Behaviors/GameCanvasMaskBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using Antique_Tycoon.Extensions;
+using Antique_Tycoon.Views.Controls;
+using Antique_Tycoon.Views.Widgets;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using Avalonia.Threading;
+using Avalonia.Xaml.Interactivity;
+using PropertyGenerator.Avalonia;
+
+namespace Antique_Tycoon.Behaviors;
+
+public class GameCanvasMaskBehavior : Behavior<GameCanvas>
+{
+    private Canvas _targetCanvas;
+
+    protected override void OnAttached()
+    {
+        base.OnAttached();
+        Dispatcher.UIThread.Post(() =>
+        {
+            var listBox = AssociatedObject.FindLogicalDescendantOfType<XListBox>();
+            var itemsPresenter = listBox.FindVisualChild<ItemsPresenter>();
+            _targetCanvas = itemsPresenter.FindLogicalDescendantOfType<Canvas>();
+            
+            var maskBorder = new Border
+            {
+                Background = Brushes.Black,
+                Opacity = 0.5,
+                ZIndex = 2,
+                Width = _targetCanvas.Bounds.Width,
+                Height = _targetCanvas.Bounds.Height
+            };
+            Canvas.SetLeft(maskBorder, 0);
+            Canvas.SetTop(maskBorder, 0);
+            _targetCanvas.Children.Add(maskBorder);
+        });
+    }
+}
--- Antique_Tycoon/Extensions/VisualExtensions.cs
+++ Antique_Tycoon/Extensions/VisualExtensions.cs
@@ -23,4 +23,28 @@ public static class VisualExtensions
     }
     throw new Exception("无法获取GetVisualRoot");
   }
+  
+  /// <summary>
+  /// 从视觉树中查找指定类型的第一个子元素（递归查找）
+  /// </summary>
+  /// <typeparam name="T">目标控件类型</typeparam>
+  /// <param name="root">查找的根控件</param>
+  /// <returns>找到的目标控件，未找到返回 null</returns>
+  public static T? FindVisualChild<T>(this Visual root) where T : Visual
+  {
+    // 根控件本身就是目标类型，直接返回
+    if (root is T target)
+      return target;
+
+    // 迭代根控件的所有视觉子元素（Avalonia 用 Visual.Children 替代 VisualTreeHelper.GetChildrenCount）
+    foreach (var child in root.GetVisualChildren())
+    {
+      // 递归查找子元素的后代
+      var descendant = child.FindVisualChild<T>();
+      if (descendant != null)
+        return descendant;
+    }
+
+    return null;
+  }
 }
